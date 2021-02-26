2053f6dac4ea4b6c786fdb7431703c3547048531
Kyle Owen <kylevowen@gmail.com>
2021-02-26 12:27:02 -0600

    SN7474 clocks data from two cycles prior to increase setup time

--- pdp8i/modules/sn7474.v
+++ pdp8i/modules/sn7474.v
@@ -9,28 +9,66 @@ module sn7474 (
 	input clr_n,
 	output reg q,
 	output reg q_n
+	//output q,
+	//output q_n
 	);
 
 reg old_clk;
-reg old_d;
+reg old_d, old_old_d;
+reg old_pre_n;
+reg old_clr_n;
+
+//reg q_int, q_n_int;
+
+/*
+assign q = (old_pre_n && old_clr_n) ? q_int :
+           (old_pre_n && !old_clr_n) ? 1'b0 : 1'b1;
+assign q_n = (old_pre_n && old_clr_n) ? q_n_int :
+             (!old_pre_n && old_clr_n) ? 1'b0 : 1'b1;
+             */
+             /*
+assign q = (pre_n && clr_n) ? q_int :
+           (pre_n && !clr_n) ? 1'b0 : 1'b1;
+assign q_n = (pre_n && clr_n) ? q_n_int :
+             (!pre_n && clr_n) ? 1'b0 : 1'b1;
+             */
+
 
 always @(posedge mclk) begin
+    old_pre_n <= pre_n;
+    old_clr_n <= clr_n;
 	old_clk <= clk;
     old_d <= d;
+    old_old_d <= old_d;
 
-	if (!pre_n && !clr_n) begin
+	if (!pre_n && !clr_n && !old_clr_n && !old_pre_n) begin
 		q <= 1'b1;
 		q_n <= 1'b1;
-	end else if (!pre_n) begin
+	end else if (!pre_n && !old_pre_n) begin
 		q <= 1'b1;
 		q_n <= 1'b0;
-	end else if (!clr_n) begin
+	end else if (!clr_n && !old_clr_n) begin
 		q <= 1'b0;
 		q_n <= 1'b1;
 	end else if (clk && !old_clk) begin
-		q <= old_d;
-		q_n <= !old_d;
+		q <= old_old_d;
+		q_n <= !old_old_d;
 	end
+    /*
+	if (!pre_n && !clr_n && !old_clr_n && !old_pre_n) begin
+		q_int <= 1'b1;
+		q_n_int <= 1'b1;
+	end else if (!pre_n && !old_pre_n) begin
+		q_int <= 1'b1;
+		q_n_int <= 1'b0;
+	end else if (!clr_n && !old_clr_n) begin
+		q_int <= 1'b0;
+		q_n_int <= 1'b1;
+	end else if (clk && !old_clk) begin
+		q_int <= old_d;
+		q_n_int <= !old_d;
+	end
+    */
 end
 /*
 always @(negedge pre_n or negedge clr_n or posedge clk) begin
